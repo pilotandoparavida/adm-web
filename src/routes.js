387d8a92a45971ae3d091d6f859e30cef3ee49df
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-06 13:33:28 -0300

    Adicionando rota base

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import EntrarPage from './pages/Entrar';
 import MainPage from './pages/Main';
 
 export default function Routes () {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL} >
             <Switch>
                 <Route path="/login" component={EntrarPage}/>
                 <Route path="/" exact component={MainPage}/>
