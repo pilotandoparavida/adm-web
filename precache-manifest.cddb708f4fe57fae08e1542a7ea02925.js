d0c41f84333ea90fedd82bd979a2c08c91d9e084
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-06 12:45:14 -0300

    Updates

---
+++ precache-manifest.cddb708f4fe57fae08e1542a7ea02925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70564263ce6d48af467f226919e5d353",
+    "url": "/adm-web/index.html"
+  },
+  {
+    "revision": "97cdc34f1401a7bdd4d5",
+    "url": "/adm-web/static/css/main.dc31365c.chunk.css"
+  },
+  {
+    "revision": "fc05253f9e128f717be9",
+    "url": "/adm-web/static/js/2.c56a1930.chunk.js"
+  },
+  {
+    "revision": "97cdc34f1401a7bdd4d5",
+    "url": "/adm-web/static/js/main.40d52232.chunk.js"
+  },
+  {
+    "revision": "d0b1081b952be2290f06",
+    "url": "/adm-web/static/js/runtime-main.f9c2da7a.js"
+  },
+  {
+    "revision": "48a7c9194350d7c0729a90b247b2a8d7",
+    "url": "/adm-web/static/media/background.48a7c919.jpg"
+  },
+  {
+    "revision": "90f64a36dcbc02f29423c3648fdd7f6c",
+    "url": "/adm-web/static/media/logo.90f64a36.png"
+  }
+]);
