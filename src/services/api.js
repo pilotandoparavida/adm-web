8b89faab1afe09753cbc325291250bef7afe8794
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-07 14:27:59 -0300

    Desenvolvimento Adaptando estrutura para ambiente de desenvolvimento.

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://adm-backend.herokuapp.com", 
-    // baseURL: "http://localhost:4444", 
+    // baseURL: "https://adm-backend.herokuapp.com", 
+    baseURL: "http://localhost:4444", 
     timeout: 30000
 });
 
